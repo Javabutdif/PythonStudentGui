e74597ad94e16a89ebb19d7356c0b076339374fd
unknown <HH225@ccs.local>
2023-10-18 17:49:20 +0100

    Frame Added

--- PythonAct3/Main.py
+++ PythonAct3/Main.py
@@ -8,6 +8,11 @@ class Student:
 		
 		self.root = Tk()
 		self.root.title('Genabio, Anton James')
+		self.root.geometry("500x400")
+		self.root.resizable(False, False)
+		self.root.eval("tk::PlaceWindow  .  center")
+		self.frame = Frame(self.root, bd=20)
+		self.frame.grid()
 		
 		#Label Rows
 		self.lb_idNo = Label(self.root, text="Id No: ")
