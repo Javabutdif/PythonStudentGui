eb23edbd1c22935767e0c9fdd8ab17f091c9e8cf
unknown <HH225@ccs.local>
2023-10-18 17:38:38 +0100

    Grid Added

--- PythonAct3/Main.py
+++ PythonAct3/Main.py
@@ -9,6 +9,18 @@ class Student:
 		self.root = Tk()
 		self.root.title('Genabio, Anton James')
 		
+		#Label Rows
+		self.lb_idNo = Label(self.root, text="Id No: ")
+		self.lb_idNo.grid(row=0,column=0)
+		self.lb_FirstName = Label(self.root, text="First Name: ")
+		self.lb_FirstName.grid(row=1,column=0)
+		#TextBox Columns
+		self.entry_idNo = Entry(self.root)
+		self.entry_idNo.grid(row=0,column=1)
+		self.entry_firstName = Entry(self.root)
+		self.entry_firstName.grid(row=1,column=1)
+		
+		
 		
 		
 		#Execution
