d8d4986deecb9aa037f991fc1d5c29dbc91d0148
unknown <HH225@ccs.local>
2023-10-18 18:10:58 +0100

    Other update

--- PythonAct3/Main.py
+++ PythonAct3/Main.py
@@ -14,16 +14,26 @@ class Student:
 		self.frame = Frame(self.root, bd=20)
 		self.frame.grid()
 		
+		self.fontModify = "Arial,30"
+		
 		#Label Rows
-		self.lb_idNo = Label(self.root, text="Id No: ")
+		self.lb_idNo = Label(self.root, text="Id No: ", font = self.fontModify )
 		self.lb_idNo.grid(row=0,column=0)
-		self.lb_FirstName = Label(self.root, text="First Name: ")
-		self.lb_FirstName.grid(row=1,column=0)
+		self.lb_LastName = Label(self.root, text="Last Name: ", font = self.fontModify )
+		self.lb_LastName.grid(row=1,column=0)
+		self.lb_FirstName = Label(self.root, text="First Name: ", font = self.fontModify )
+		self.lb_FirstName.grid(row=2,column=0)
+		self.lb_course = Label(self.root, text="Course: ", font = self.fontModify )
+		self.lb_course.grid(row=3,column=0)
+		self.lb_year = Label(self.root, text="Year: ", font = self.fontModify )
+		self.lb_year.grid(row=4,column=0)
 		#TextBox Columns
-		self.entry_idNo = Entry(self.root)
+		self.entry_idNo = Entry(self.root, font = self.fontModify  )
 		self.entry_idNo.grid(row=0,column=1)
-		self.entry_firstName = Entry(self.root)
-		self.entry_firstName.grid(row=1,column=1)
+		self.entry_lastName = Entry(self.root, font = self.fontModify  )
+		self.entry_lastName.grid(row=1,column=1)
+		self.entry_firstName = Entry(self.root, font = self.fontModify  )
+		self.entry_firstName.grid(row=2,column=1)
 		
 		
 		
