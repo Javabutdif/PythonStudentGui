e22378a9630a6d5de8300d1074528704fa412f6f
unknown <HH225@ccs.local>
2023-10-18 17:24:26 +0100

    first commit

---
+++ PythonAct3/Main.py
@@ -0,0 +1,24 @@
+
+from tkinter import *
+
+
+class Student:
+	
+	def __init__ (self):
+		
+		self.root = Tk()
+		self.root.title('Genabio, Anton James')
+		
+		
+		
+		#Execution
+		self.root.mainloop()
+
+
+def main()->None:
+	Student()
+
+
+
+if __name__ == "__main__":
+	main()
